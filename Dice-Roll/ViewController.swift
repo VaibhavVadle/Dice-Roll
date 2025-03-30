8bbf9a99f3ac2e8e3fff0ce2763f208bed6adc88
VaibhavVadle <vaibhavvadle09@gmail.com>
2025-03-30 23:17:51 +0530

    Added random dice number image view

--- Dice-Roll/ViewController.swift
+++ Dice-Roll/ViewController.swift
@@ -5,15 +5,20 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        diceImageView1.image = UIImage(imageLiteralResourceName: "DiceTwo")
-        diceImageView2.image = UIImage(imageLiteralResourceName: "DiceSix")
-    }
+    var leftDiceNumber = 1
+    var rightDiceNumber = 5
+    let listOfDiceImages: [UIImage] = [
+        UIImage(imageLiteralResourceName: "DiceOne"),
+        UIImage(imageLiteralResourceName: "DiceTwo"),
+        UIImage(imageLiteralResourceName: "DiceThree"),
+        UIImage(imageLiteralResourceName: "DiceFour"),
+        UIImage(imageLiteralResourceName: "DiceFive"),
+        UIImage(imageLiteralResourceName: "DiceSix"),
+    ]
     
     @IBAction func rollDicePressed(_ sender: UIButton) {
-        diceImageView1.image = UIImage(imageLiteralResourceName: "DiceFour")
-        diceImageView2.image = UIImage(imageLiteralResourceName: "DiceFour")
+        diceImageView1.image =  listOfDiceImages.randomElement()
+        diceImageView2.image =  listOfDiceImages.randomElement()
     }
 }
 
